d3635cd6b62c522288c26b2e7fffb8c8de10d6e3
jaredasch1 <jaredasch1@gmail.com>
2019-01-31 22:14:47 -0500

    new image

--- picmaker.c
+++ picmaker.c
@@ -5,18 +5,22 @@
 #include <unistd.h>
 
 int main(){
-  char header[20] = "P3 500 500 255 ";
+  char header[20] = "P3 1000 1000 255 ";
   int pixel[3];
   char line[20];
-  
+
   int fd = open("image.ppm", O_WRONLY | O_CREAT | O_TRUNC, 0644);
   write(fd, header, strlen(header));
-  for(int i = 0; i < 500; i++){
-    for(int j = 0; j < 500; j++){
-      snprintf(line, 20, "%d %d %d ", i, j, 0);
+  for(int i = 0; i < 1000; i++){
+    for(int j = 0; j < 1000; j++){
+      pixel[0] = j % (i + 1);
+      pixel[1] = i % (j + 1);
+      pixel[2] = 256 / ((i+1) * (j+1));
+      snprintf(line, 20, "%d %d %d ", pixel[0], pixel[1], pixel[2]);
       write(fd, line, strlen(line));
     }
   }
+  close(fd);
 
   return 0;
 }
